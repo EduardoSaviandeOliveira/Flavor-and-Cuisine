8eebc815a5b3be5136ed7d619f2f1f84a851a72d
Eduardo-Savian-de-Oliveira <eduardosaviandeoliveira@proton.me>
2022-12-01 16:15:03 -0300

    Added coms

--- application/app.py
+++ application/app.py
@@ -8,10 +8,24 @@ from auth.auth_bearer import JWTBearer
 from auth.auth_handler import signJWT
 from database import engine, get_db
 from exceptions import UserException, RecipeException, CommentException
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Authentication
