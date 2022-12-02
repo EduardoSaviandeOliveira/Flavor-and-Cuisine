3f29c0a5c4b8e90ab954f0ff3857dfa5baae0a0c
marcs-feh <marcosfehlauer@protonmail.com>
2022-12-02 16:26:05 -0300

    add option to alternate between login and signup

--- front_end/src/App.svelte
+++ front_end/src/App.svelte
@@ -4,10 +4,23 @@
   import SignUp from './lib/SignUp.svelte';
   import Login from './lib/Login.svelte';
   import AllRecipes from './lib/AllRecipes.svelte';
+  
+  let showSignUp = false;
 </script>
 
 <main>
+{#if showSignUp}
 <SignUp></SignUp>
+<div id="log-in-msg">
+Already have an account? 
+<button type="submit" on:click={() => showSignUp = false}>Log in</button>
+</div>
+{:else}
 <Login></Login>
+<div id="create-account-msg">
+Don't have an account? 
+<button type="submit" on:click={() => showSignUp = true}>Create Account</button>
+</div>
+{/if}
 <AllRecipes></AllRecipes>
 </main>
