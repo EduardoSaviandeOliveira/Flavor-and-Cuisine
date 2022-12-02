b3025263c6087c960f8e257d8c662cae25acbacf
Eduardo-Savian-de-Oliveira <eduardosaviandeoliveira@proton.me>
2022-12-02 17:17:41 -0300

    Added favicon

--- front_end/src/App.svelte
+++ front_end/src/App.svelte
@@ -1,6 +1,4 @@
 <script>
-  import { FLAVOR_CUISINE_URL } from './stores.js'
-  import Counter from './lib/Counter.svelte'
   import SignUp from './lib/SignUp.svelte';
   import Login from './lib/Login.svelte';
   import AllRecipes from './lib/AllRecipes.svelte';
